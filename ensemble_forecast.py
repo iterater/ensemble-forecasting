21b226f20a1002841a58933b902cff02af0f545b
Sergey V. Kovalchuk <sergey.v.kovalchuk@gmail.com>
2015-07-21 17:54:00 +0300

    Ensemble error estimation

--- ensemble_forecast.py
+++ ensemble_forecast.py
@@ -1,13 +1,22 @@
 import numpy as np
-from sklearn import tree
+from sklearn import svm
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
 N = 430
 T = 48
 
-# preparing measurements forecast
+# preparing measurements data
 m = np.loadtxt('data/2011/2011080100_measurements_GI_2623.txt')
+MHT = 70  # maximum history length
+mh = np.zeros((N, MHT))
+for i in range(N):
+    for j in range(MHT):
+        tt = i*6-j
+        if tt < 0:
+            mh[i, j] = np.nan
+        else:
+            mh[i, j] = m[tt]
 
 # ensembles loading
 with open('data/2011/2011080100_ens_dist_names.txt') as t_file:
@@ -20,7 +29,7 @@ for i in range(len(ens_names)):
     print('Loaded', ens_names[i], np.shape(ens_fc[i]), np.shape(ens_err[i]))
 ens_dist_dtw = np.loadtxt('data/2011/2011080100_ens_dist_dtw.txt').transpose()
 ens_dist_mae = np.loadtxt('data/2011/2011080100_ens_dist_mae.txt').transpose()
-ens_dist_index = np.loadtxt('data/2011/2011080100_ens_dist_index.txt')
+ens_dist_index = np.loadtxt('data/2011/2011080100_ens_dist_index.txt').astype(int)
 ens_dist_index_matrix = np.full((len(ens_names), len(ens_names)), -1)
 for i in range(len(ens_dist_index)):
     ens_dist_index_matrix[ens_dist_index[i, 0], ens_dist_index[i, 1]] = i
@@ -34,29 +43,52 @@ for t in range(N):
 best_class = np.array(best_class).astype(int)
 print(best_class)
 
-# classification try
+# error prediction
 train_ratio = 0.6
+history_view_window = 24
+available_start = int(np.ceil(history_view_window/6))
 for i in range(10):
-    shuffled_index = np.arange(N)
+    shuffled_index = np.arange(N - available_start) + available_start
     np.random.shuffle(shuffled_index)
     train_index = shuffled_index[0:int(train_ratio*N)]
     test_index = shuffled_index[int(train_ratio*N):]
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(ens_dist_dtw[train_index], best_class[train_index])
-    prediction = clf.predict(ens_dist_dtw[test_index])
-    full_err = np.mean(ens_err[-1][test_index, 1])
-    predicted_error = np.mean([ens_err[prediction[j]][test_index[j], 1] for j in range(len(prediction))])
-    print('DIFF:', full_err - predicted_error)
+    predicted_err = []
+    for ens_i in range(len(ens_names)):
+        # ens_idx_selection = (ens_dist_index[:, 0].flatten() == ens_i) | (ens_dist_index[:, 1].flatten() == ens_i)
+        train_arg = np.hstack((mh[train_index, 0:history_view_window], ens_dist_dtw[train_index]))
+        test_arg = np.hstack((mh[test_index, 0:history_view_window], ens_dist_dtw[test_index]))
+        train_val = ens_err[ens_i][train_index, 1]
+        regr = svm.SVR()
+        regr.fit(train_arg, train_val)
+        predicted_err += [regr.predict(test_arg), ]
+    predicted_err = np.array(predicted_err).transpose()
+    ens_selection = np.argmin(predicted_err, axis=1)
+    err_selection = [ens_err[ens_selection[j]][test_index[j], 1] for j in range(len(ens_selection))]
+    print('DIFF: ', np.mean(ens_err[-1][test_index, 1]) - np.mean(err_selection))
+
+
+
+# classification try
+# train_ratio = 0.6
+# for i in range(10):
+#     shuffled_index = np.arange(N)
+#     np.random.shuffle(shuffled_index)
+#     train_index = shuffled_index[0:int(train_ratio*N)]
+#     test_index = shuffled_index[int(train_ratio*N):]
+#     clf = tree.DecisionTreeClassifier()
+#     clf = clf.fit(ens_dist_dtw[train_index], best_class[train_index])
+#     prediction = clf.predict(ens_dist_dtw[test_index])
+#     full_err = np.mean(ens_err[-1][test_index, 1])
+#     predicted_error = np.mean([ens_err[prediction[j]][test_index[j], 1] for j in range(len(prediction))])
+#     print('DIFF:', full_err - predicted_error)
 
 # pca on distance vector
-pca = PCA(n_components=2)
-pca_res = pca.fit_transform(ens_dist_dtw)
-pca = PCA(n_components=2)
-pca_res = pca.fit_transform(ens_dist_dtw)
-colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
-plt.xlabel('All-distance PC#1')
-plt.ylabel('All-distance PC#2')
-for i in range(7):
-    plt.plot(pca_res[best_class == i, 0], pca_res[best_class == i, 1], 'o'+colors[i])
-plt.savefig('pics\\2011\\all-distance-pca.png')
-plt.close()
+# pca = PCA(n_components=2)
+# pca_res = pca.fit_transform(ens_dist_dtw)
+# colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+# plt.xlabel('All-distance PC#1')
+# plt.ylabel('All-distance PC#2')
+# for i in range(7):
+#     plt.plot(pca_res[best_class == i, 0], pca_res[best_class == i, 1], 'o'+colors[i])
+# plt.savefig('pics\\2011\\all-distance-pca.png')
+# plt.close()
